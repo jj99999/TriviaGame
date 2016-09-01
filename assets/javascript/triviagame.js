2ea76e3893c0bb269f2f2ec3ca45ceb04642e5be
jj99999 <jonathan.jackson3@gmail.com>
2016-08-31 22:37:11 -0400

    final files, need  styling

--- assets/javascript/triviagame.js
+++ assets/javascript/triviagame.js
@@ -1,32 +1,78 @@
 //javascript for Trivia Game
 
 
+var count=10;
 
-var count=30;
+var counter=setInterval(countDown, 1000);
 
-var counter=setInterval(timer, 1000);
+var correctCount=0;
 
-function countDown(){
+
+function countDown()
+{
   count=count-1;
+  console.log(count);
   if (count <= 0)
   {
-     clearInterval(counter);
-     alert("Time's up!");
-     return;
+    $(".panel-title").html("Time's Up!")
+    clearInterval(counter);
+    $("input[type=radio]").attr('disabled', true);
+    $("#results").html(correctCount + " correct answers!")
   }
-
-  	document.getElementById("timer").innerHTML=count + " secs";
+  $("#timer").html(count + " seconds left");
 }
 
 
+function answerCheck()
+{
+
+  $('#answer1').click(function ()
+  {
+    correctCount++;
+    if (this.id == 'answer1')
+    {
+      $("#response1").html("That's right! Wrangell-St Elias is over 8 million acres!");
+    }  
+  });
+
+  $('#answer2').click(function ()
+  {
+    correctCount++;
+    if (this.id == 'answer2')
+    {
+      $("#response2").html("That's right! President Wilson created the National Park Service in 1916.");
+    }  
+  });
+
+  $('#answer3').click(function ()
+  {
+    correctCount++;
+    if (this.id == 'answer3')
+    {
+      $("#response3").html("Trick question! You can find all these things in National Parks!");
+    }  
+  });
+
+  $('#answer4').click(function ()
+  {
+    correctCount++;
+    if (this.id == 'answer4')
+    {
+      $("#response3").html("Trick question! You can find all these things in National Parks!");
+    }  
+  });
+
+  $('#answer5').click(function ()
+  {
+    correctCount++;
+    if (this.id == 'answer5')
+    {
+      $("#response3").html("Trick question! You can find all these things in National Parks!");
+    }  
+  });
+
+}
 
 
-
-
-
-
-
-		
-
-	
-
+countDown();
+answerCheck();
